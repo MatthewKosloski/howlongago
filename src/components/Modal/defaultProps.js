27d7d8ecb3fecf6e57fe8263f7da4579165ef79d
MatthewKosloski <matthewkosloski@me.com>
2017-06-09 01:05:26 -0500

    changed Modal animation to scale

--- src/components/Modal/defaultProps.js
+++ src/components/Modal/defaultProps.js
@@ -6,24 +6,24 @@ const defaultProps = {
 	animation: {
 		componentWillEnter: {
 			fromVars: {
-				y: 100,
+				scale: 0,
 				opacity: 0
 			},
 			toVars: {
-				y: 0,
+				scale: 1,
 				opacity: 1,
-				ease: Back.easeInOut.config(4)
+				ease: Back.easeInOut.config(1)
 			}
 		},
 		componentWillLeave: {
 			fromVars: {
-				y: 0,
+				scale: 1,
 				opacity: 1
 			},
 			toVars: {
-				y: 100,
+				scale: 0,
 				opacity: 0,
-				ease: Back.easeInOut.config(4)
+				ease: Back.easeInOut.config(1)
 			}
 		}
 	}
